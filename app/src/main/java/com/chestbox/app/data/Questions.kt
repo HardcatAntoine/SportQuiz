31136978bc5947a86bb9b8c96faf170cc3a779d9
HardcatAntoine <br41nk1ck3r@gmail.com>
2023-10-26 17:19:27 +0300

    other files

---
+++ app/src/main/java/com/chestbox/app/data/Questions.kt
@@ -0,0 +1,14 @@
+package com.chestbox.app.data
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Questions(
+    @SerializedName("question") @Expose val question: String? = null,
+    @SerializedName("a") @Expose val a: String? = null,
+    @SerializedName("b") @Expose val b: String? = null,
+    @SerializedName("c") @Expose val c: String? = null,
+    @SerializedName("d") @Expose val d: String? = null,
+    @SerializedName("answer") @Expose val answer: String? = null,
+
+    )
