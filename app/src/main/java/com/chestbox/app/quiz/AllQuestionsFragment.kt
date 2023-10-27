8dc7bed69d9e35354657755527273ef6b68e4d44
HardcatAntoine <br41nk1ck3r@gmail.com>
2023-10-27 10:30:19 +0300

    AllQuestionsFragment realization layout & logic

--- app/src/main/java/com/chestbox/app/quiz/AllQuestionsFragment.kt
+++ app/src/main/java/com/chestbox/app/quiz/AllQuestionsFragment.kt
@@ -1,18 +1,238 @@
 package com.chestbox.app.quiz
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.chestbox.app.R
+import com.chestbox.app.data.Questions
+import com.chestbox.app.databinding.FragmentAllQuestionsBinding
+import com.chestbox.app.databinding.FragmentHockeyBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.IOException
 
-class AllQuestionsFragment : Fragment() {
+class AllQuestionsFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding: FragmentAllQuestionsBinding
+    private var mCurrentPosition: Int = 1 // Default and the first question position
+    private var mQuestionsList: List<Questions>? = null
 
+    private var mSelectedOptionPosition: Int = 0
+    private var mCorrectAnswers: Int = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_all_questions, container, false)
+        binding = FragmentAllQuestionsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mQuestionsList = getAllQuestions(requireContext())
+
+        setQuestion()
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+
+            R.id.tv_option_one -> {
+
+                selectedOptionView(binding.tvOptionOne, 1)
+            }
+
+            R.id.tv_option_two -> {
+
+                selectedOptionView(binding.tvOptionTwo, 2)
+            }
+
+            R.id.tv_option_three -> {
+
+                selectedOptionView(binding.tvOptionThree, 3)
+            }
+
+            R.id.tv_option_four -> {
+
+                selectedOptionView(binding.tvOptionFour, 4)
+            }
+
+            R.id.btn_submit -> {
+
+                if (mSelectedOptionPosition == 0) {
+
+                    mCurrentPosition++
+
+                    when {
+
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+
+                            setQuestion()
+                        }
+
+                        else -> {
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+                    // This is to check if the answer is wrong
+                    if (question!!.answer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswers++
+                    }
+
+                    // This is for correct answer
+                    answerView(question.answer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size) {
+                        binding.btnSubmit.text = "FINISH"
+
+                    } else {
+                        binding.btnSubmit.text = "GO TO NEXT QUESTION"
+                    }
+
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    /**
+     * A function for setting the question to UI components.
+     */
+    private fun setQuestion() {
+
+        val question =
+            mQuestionsList!!.get(mCurrentPosition - 1) // Getting the question from the list with the help of current position.
+
+        defaultOptionsView()
+
+        if (mCurrentPosition == mQuestionsList!!.size) {
+            binding.btnSubmit.text = "FINISH"
+            binding.btnSubmit.setOnClickListener {
+                val action =
+                    BasketballFragmentDirections.actionBasketballFragmentToResultFragment(
+                        mCorrectAnswers
+                    )
+                findNavController().navigate(action)
+            }
+        } else {
+            binding.btnSubmit.text = "SUBMIT"
+        }
+
+        binding.progressBar.progress = mCurrentPosition
+        binding.tvProgress.text = "$mCurrentPosition" + "/" + binding.progressBar.max
+
+        binding.tvQuestion.text = question.question
+        binding.tvOptionOne.text = question.a
+        binding.tvOptionTwo.text = question.b
+        binding.tvOptionThree.text = question.c
+        binding.tvOptionFour.text = question.d
+    }
+
+    /**
+     * A function to set the view of selected option view.
+     */
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+
+        defaultOptionsView()
+
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(
+            Color.parseColor("#363A43")
+        )
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            requireContext(),
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+    /**
+     * A function to set default options view when the new question is loaded or when the answer is reselected.
+     */
+    private fun defaultOptionsView() {
+
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    /**
+     * A function for answer view which is used to highlight the answer is wrong or right.
+     */
+    private fun answerView(answer: Int, drawableView: Int) {
+
+        when (answer) {
+
+            1 -> {
+                binding.tvOptionOne.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    drawableView
+                )
+            }
+
+            2 -> {
+                binding.tvOptionTwo.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    drawableView
+                )
+            }
+
+            3 -> {
+                binding.tvOptionThree.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    drawableView
+                )
+            }
+
+            4 -> {
+                binding.tvOptionFour.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    drawableView
+                )
+            }
+        }
+    }
+    fun getAllQuestions(context: Context): List<Questions> {
+        lateinit var jsonString: String
+        try {
+            jsonString =
+                context.assets.open("all/all.json").bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            Log.d("Exception", ioException.message.toString())
+        }
+        val listSample = object : TypeToken<List<Questions>>() {}.type
+        return Gson().fromJson(jsonString, listSample)
     }
 }
