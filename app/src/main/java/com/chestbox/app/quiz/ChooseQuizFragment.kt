9b8614f60b35becbd69b0740238abf742b2076f1
HardcatAntoine <br41nk1ck3r@gmail.com>
2023-10-27 10:05:31 +0300

    ChooseQuizFragment realization layout & logic

--- app/src/main/java/com/chestbox/app/quiz/ChooseQuizFragment.kt
+++ app/src/main/java/com/chestbox/app/quiz/ChooseQuizFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.chestbox.app.R
+import com.chestbox.app.databinding.FragmentChooseQuizBinding
 
 
 class ChooseQuizFragment : Fragment() {
-
+    lateinit var binding: FragmentChooseQuizBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_quiz, container, false)
+        binding = FragmentChooseQuizBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.bascketballBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_chooseQuizFragment_to_basketballFragment)
+        }
+        binding.boxingBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_chooseQuizFragment_to_boxingFragment)
+        }
+        binding.hockeyBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_chooseQuizFragment_to_hockeyFragment)
+        }
+        binding.allBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_chooseQuizFragment_to_allQuestionsFragment)
+        }
     }
 }
