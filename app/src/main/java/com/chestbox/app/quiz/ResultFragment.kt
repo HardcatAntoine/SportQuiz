7776adb6f116f66405d1a27316306a4e4cb1df3b
HardcatAntoine <br41nk1ck3r@gmail.com>
2023-10-27 10:30:48 +0300

    ResultFragment realization layout & logic

--- app/src/main/java/com/chestbox/app/quiz/ResultFragment.kt
+++ app/src/main/java/com/chestbox/app/quiz/ResultFragment.kt
@@ -5,14 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.chestbox.app.R
+import com.chestbox.app.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
+    private lateinit var binding: FragmentResultBinding
+    private val args: ResultFragmentArgs by lazy {
+        ResultFragmentArgs.fromBundle(requireArguments())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        binding = FragmentResultBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val score = args.mCorrectAnswers
+        binding.tvScore.text = "Your Score is ${score}"
+        binding.btnFinish.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_chooseQuizFragment)
+        }
     }
 }
